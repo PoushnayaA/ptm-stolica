73d2af0eff1b66159e16e5c6575439e505cc1586
PoushnayaA <poushnaya@bk.ru>
2024-03-22 00:07:17 +0300

    UPD

--- source/js/main.js
+++ source/js/main.js
@@ -2,14 +2,42 @@ import { iosVhFix } from './utils/ios-vh-fix';
 import { initRentSlider } from './modules/init-rent-slider';
 import { initSaleSlider } from './modules/init-sale-slider';
 
+import { initAccordions } from './modules/accordion/init-accordion';
+
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
   initRentSlider();
   initSaleSlider();
+  initAccordions();
 
   iosVhFix();
 
+  const scrollToSection = (link, section) => {
+    if (link) {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        document.getElementById('sidebar').classList.remove('active');
+        document.querySelector('.wrapper').classList.remove('dark');
+      });
+    }
+  };
+
+  const form = document.querySelector('#form');
+  const vacancies = document.querySelector('#vacancies');
+  const extra = document.querySelector('#extra');
+  const contacts = document.querySelector('#contacts');
+  const product1 = document.querySelector('#type-1');
+  const product2 = document.querySelector('#type-2');
+
+  scrollToSection(document.querySelector('a[href="#form"]'), form);
+  scrollToSection(document.querySelector('a[href="#vacancies"]'), vacancies);
+  scrollToSection(document.querySelector('a[href="#extra"]'), extra);
+  scrollToSection(document.querySelector('a[href="#contacts"]'), contacts);
+
+  scrollToSection(document.querySelector('a[href="#type-1"]'), product1);
+  scrollToSection(document.querySelector('a[href="#type-2"]'), product2);
 
   function equalizeHeights() {
     const items = document.querySelectorAll('.products__item');
@@ -42,53 +70,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   window.addEventListener('load', equalizeHeights);
   window.addEventListener('resize', equalizeHeights);
-
-  const form = document.querySelector('#form');
-  const vacancies = document.querySelector('#vacancies');
-  const extra = document.querySelector('#extra');
-  const contacts = document.querySelector('#contacts');
-
-  const formLink = document.querySelector('a[href="#form"]');
-  formLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    form.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    });
-  });
-
-  const vacanciesLink = document.querySelector('a[href="#vacancies"]');
-  vacanciesLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    vacancies.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    });
-    document.getElementById('sidebar').classList.remove('active');
-    document.querySelector('.wrapper').classList.remove('dark');
-  });
-
-  const extraLink = document.querySelector('a[href="#extra"]');
-  extraLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    extra.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    });
-    document.getElementById('sidebar').classList.remove('active');
-    document.querySelector('.wrapper').classList.remove('dark');
-  });
-
-  const contactsLink = document.querySelector('a[href="#contacts"]');
-  contactsLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    contacts.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    });
-    document.getElementById('sidebar').classList.remove('active');
-    document.querySelector('.wrapper').classList.remove('dark');
-  });
 
   const navigation = document.querySelector('.main-header__navigation');
   function clickOutside() {
