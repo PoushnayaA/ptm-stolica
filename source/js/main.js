6f4a44a6261c8179a202cfd17a924f3af6e9edb6
PoushnayaA <poushnaya@bk.ru>
2024-03-21 17:22:08 +0300

    UPD

--- source/js/main.js
+++ source/js/main.js
@@ -18,7 +18,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
     items.forEach(item => {
       const header = item.querySelector('h2');
-      header.style.height = 'auto'; // Сбрасываем высоту заголовка перед пересчетом
+      header.style.height = 'auto';
+
+      item.style.height = 'auto';
+    });
+
+    items.forEach(item => {
+      const header = item.querySelector('h2');
+      header.style.height = 'auto';
+      item.style.height = 'auto';
       const headerHeight = header.offsetHeight;
       const itemHeight = item.offsetHeight;
 
@@ -32,6 +40,54 @@ window.addEventListener('DOMContentLoaded', () => {
       item.style.height = `${maxItemHeight}px`;
     });
   }
+
+const form = document.querySelector('#form');
+const vacancies = document.querySelector('#vacancies');
+const extra = document.querySelector('#extra');
+const contacts = document.querySelector('#contacts');
+
+const formLink = document.querySelector('a[href="#form"]');
+
+formLink.addEventListener('click', function(e) {
+  e.preventDefault();
+  form.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+});
+
+const vacanciesLink = document.querySelector('a[href="#vacancies"]');
+vacanciesLink.addEventListener('click', function(e) {
+  e.preventDefault();
+  vacancies.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+  document.getElementById('sidebar').classList.remove('active');
+  document.querySelector('.wrapper').classList.remove('dark');
+});
+
+const extraLink = document.querySelector('a[href="#extra"]');
+extraLink.addEventListener('click', function(e) {
+  e.preventDefault();
+  extra.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+  document.getElementById('sidebar').classList.remove('active');
+  document.querySelector('.wrapper').classList.remove('dark');
+});
+
+const contactsLink = document.querySelector('a[href="#contacts"]');
+contactsLink.addEventListener('click', function(e) {
+  e.preventDefault();
+  contacts.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+  document.getElementById('sidebar').classList.remove('active');
+  document.querySelector('.wrapper').classList.remove('dark');
+});
 
   window.addEventListener('load', equalizeHeights);
   window.addEventListener('resize', equalizeHeights);
